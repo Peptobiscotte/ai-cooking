10c8cb6add6cab544f2a826250ef1da2a6bced4b
Pepto <monsieurfranci@gmail.com>
2025-01-28 16:57:10 +0100

    mobile responsiveness

--- components/gemini/course.tsx
+++ components/gemini/course.tsx
@@ -15,9 +15,9 @@ export default function Course({ isCourse, setIsCourse}: CourseProps) {
 
     return (
         <div className="flex justify-center gap-12 py-16">
-            <button id="starter" onClick={handleCourse} className={`text-zinc-950 text-4xl font-bold  rounded-md w-48 h-48 flex items-center justify-center hover:scale-125 transition ease-in-out duration-300 bg-starter bg-cover ${isCourse === 'starter' ? 'scale-125': ''}`}><p>Starter</p></button>
-            <button id="main" onClick={handleCourse} className={`text-zinc-950 text-4xl font-bold  rounded-md w-48 h-48 flex items-center justify-center hover:scale-125 transition ease-in-out duration-300 bg-main bg-cover ${isCourse === 'main' ? 'scale-125': ''}`}><p>Main</p></button>
-            <button id="dessert" onClick={handleCourse} className={`text-zinc-950 text-4xl font-bold  rounded-md w-48 h-48 flex items-center justify-center hover:scale-125 transition ease-in-out duration-300 bg-dessert bg-cover ${isCourse === 'dessert' ? 'scale-125': ''}`}><p>Dessert</p></button>
+            <button id="starter" onClick={handleCourse} className={`text-zinc-950  font-bold  rounded-md w-24 h-24 md:w-48 md:h-48 flex items-center justify-center hover:scale-125 transition ease-in-out duration-300 bg-starter bg-cover ${isCourse === 'starter' ? 'scale-125': ''}`}><p className="text-xl md:text-4xl">Starter</p></button>
+            <button id="main" onClick={handleCourse} className={`text-zinc-950  font-bold  rounded-md w-24 h-24 md:w-48 md:h-48 flex items-center justify-center hover:scale-125 transition ease-in-out duration-300 bg-main bg-cover ${isCourse === 'main' ? 'scale-125': ''}`}><p className="text-xl md:text-4xl">Main</p></button>
+            <button id="dessert" onClick={handleCourse} className={`text-zinc-950  font-bold  rounded-md w-24 h-24 md:w-48 md:h-48 flex items-center justify-center hover:scale-125 transition ease-in-out duration-300 bg-dessert bg-cover ${isCourse === 'dessert' ? 'scale-125': ''}`}><p className="text-xl md:text-4xl">Dessert</p></button>
         </div>
     )
 }
