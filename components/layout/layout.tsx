7a5efbdee00b2dbef32b929b7073b0eea1f98aa4
Pepto <monsieurfranci@gmail.com>
2025-01-28 16:47:58 +0100

    changes to layout style

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,23 +1,12 @@
 'use client'
 
-import Image from "next/image"
-import { useRouter } from "next/navigation"
-
 export default function Layout() {
-const router = useRouter()
-
-const goToHome = function() {
-    router.push('/')
-}
 
 
     return (
-        <div className="sticky top-0 flex justify-start border-b border-zinc-800 bg-zinc-950 z-50">
-            <div className="mx-64 flex">
-                <div onClick={goToHome} className="flex items-center gap-2 border-r border-zinc-800 pr-6 cursor-pointer">
-                    <Image alt="dino" src='/ramen.svg' width={20} height={20} className="w-12 py-2"></Image>
-                    <p className="text-md font-bold">What&apos;s for dinner?</p>
-                </div>
+        <div className="sticky top-0 flex justify-center border-b border-zinc-800 bg-zinc-950 z-50 opacity-95">
+            <div className="flex items-center gap-2 py-6">
+                <p className="text-4xl font-bold">What&apos;s for dinner?</p>
             </div>
         </div>
     )
